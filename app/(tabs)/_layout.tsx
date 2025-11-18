d2d701f6a85211e82bc0b074be96e0da91d40a53
M-Bilal-Naveed <ee5985155@gmail.com>
2025-11-18 18:46:11 +0500

    Fonts added

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+
+const tabLayout = () =>{
+    return(
+        <Stack screenOptions={{headerShown: false}}>
+            <Stack.Screen name="" options={{}} />
+            <Stack.Screen name="" options={{}} />
+            <Stack.Screen name="" options={{}} />
+            <Stack.Screen name="" options={{}} />
+        </Stack>
+    )
+}
+
+export default tabLayout
